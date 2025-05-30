4e5f2ff57eabaaa8e9b2a7cdb460b5fe47fcdf28
quantinium03 <quant@quantinium.dev>
2025-05-30 11:40:42 +0530

    fixed mdsev

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
+    import Navbar from '../components/Navbar.svelte';
 	import '../app.css';
 
 	let { children } = $props();
 </script>
 
+<div class="border-b">
+    <Navbar />
+</div>
 {@render children()}
+
--- svelte.config.js
+++ svelte.config.js
@@ -2,10 +2,23 @@ import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+const mdsvexOptions = {
+    extensions: ['.md'],
+    remarkPlugins: [[remarkToc, { tight: true }]],
+    rehypePlugins: [rehypeSlug]
+}
+
 const config = {
-	preprocess: [vitePreprocess(), mdsvex()],
-	kit: { adapter: adapter() },
-	extensions: ['.svelte', '.svx']
+    preprocess: [vitePreprocess(), mdsvex(mdsvexOptions)],
+    kit: {
+        adapter: adapter(),
+        alias: {
+            '$components': './src/components/',
+            '$content': './src/content/',
+            '$lib': './src/lib',
+        },
+    },
+    extensions: ['.svelte', '.svx', '.md'],
 };
 
 export default config;
