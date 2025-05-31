7701b8599cf6fd16121209977dd0cc5032f10202
quantinium03 <quant@quantinium.dev>
2025-06-01 03:48:38 +0530

    hmm

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -2,11 +2,51 @@
 	import { fly } from 'svelte/transition';
 	import { Moon, Sun } from '@lucide/svelte';
 	import { theme } from '$lib/theme.svelte';
+
+	let links = [
+		{
+			name: 'home',
+			href: '/'
+		},
+		{
+			name: 'blog',
+			href: '/blog'
+		},
+		{
+			name: 'projects',
+			href: '/project'
+		},
+		{
+			name: 'guides',
+			href: '/guide'
+		},
+		{
+			name: 'resources',
+			href: '/resource'
+		},
+        {
+            name: 'frens',
+            href: '/fren'
+        }
+	];
 </script>
 
-<div class="dark:text-white flex justify-between max-w-4xl mx-auto text-xl py-3 border-b border-dashed px-3">
-	<div>&gt; quant</div>
-	<div>
+<div class="my-5 flex flex-col justify-between py-3 dark:text-white">
+		<pre class="text-[0.4rem] lg:text-xs leading-none text-center">
+  █████   █    ██  ▄▄▄       ███▄    █ ▄▄▄█████▓ ██▓ ███▄    █  ██▓ █    ██  ███▄ ▄███▓
+▒██▓  ██▒ ██  ▓██▒▒████▄     ██ ▀█   █ ▓  ██▒ ▓▒▓██▒ ██ ▀█   █ ▓██▒ ██  ▓██▒▓██▒▀█▀ ██▒
+▒██▒  ██░▓██  ▒██░▒██  ▀█▄  ▓██  ▀█ ██▒▒ ▓██░ ▒░▒██▒▓██  ▀█ ██▒▒██▒▓██  ▒██░▓██    ▓██░
+░██  █▀ ░▓▓█  ░██░░██▄▄▄▄██ ▓██▒  ▐▌██▒░ ▓██▓ ░ ░██░▓██▒  ▐▌██▒░██░▓▓█  ░██░▒██    ▒██ 
+░▒███▒█▄ ▒▒█████▓  ▓█   ▓██▒▒██░   ▓██░  ▒██▒ ░ ░██░▒██░   ▓██░░██░▒▒█████▓ ▒██▒   ░██▒
+░░ ▒▒░ ▒ ░▒▓▒ ▒ ▒  ▒▒   ▓▒█░░ ▒░   ▒ ▒   ▒ ░░   ░▓  ░ ▒░   ▒ ▒ ░▓  ░▒▓▒ ▒ ▒ ░ ▒░   ░  ░
+ ░ ▒░  ░ ░░▒░ ░ ░   ▒   ▒▒ ░░ ░░   ░ ▒░    ░     ▒ ░░ ░░   ░ ▒░ ▒ ░░░▒░ ░ ░ ░  ░      ░
+   ░   ░  ░░░ ░ ░   ░   ▒      ░   ░ ░   ░       ▒ ░   ░   ░ ░  ▒ ░ ░░░ ░ ░ ░      ░   
+    ░       ░           ░  ░         ░           ░           ░  ░     ░            ░   
+    </pre>
+	<div class="text-md lg:text-lg flex flex-wrap gap-3 justify-center">
+		{#each links as link (link)}
+			<span>[<a href={link.href} class="hover:underline hover:underline-offset-4">{link.name}</a>]</span>
+		{/each}
 		<button onclick={theme.toggle} aria-label="Toggle theme">
 			{#if theme.current === 'dark'}
 				<div in:fly={{ y: 10 }}>
