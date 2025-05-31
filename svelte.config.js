b4d62ad2014df87811dad580b64a00d9d29914a5
quantinium03 <quant@quantinium.dev>
2025-06-01 00:35:51 +0530

    added theming

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,27 @@
 import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import remarkToc from "remark-toc";
+import rehypeSlug from "rehype-slug"
+
+
+const mdsvexOptions = {
+    extensions: ['.md'],
+    remarkPlugins: [[remarkToc, { tight: true }]],
+    rehypePlugins: [rehypeSlug]
+}
 
 const config = {
-	preprocess: [vitePreprocess(), mdsvex()],
-	kit: { adapter: adapter() },
-	extensions: ['.svelte', '.svx']
+    preprocess: [vitePreprocess(), mdsvex(mdsvexOptions)],
+    kit: {
+        adapter: adapter(),
+        alias: {
+            '$components': './src/components/',
+            '$content': './src/content/'
+        },
+
+    },
+    extensions: ['.svelte', '.svx', '.md']
 };
 
 export default config;
