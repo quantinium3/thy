7a33c9d97bc30723d48e9f53249dd871294e33a9
quantinium03 <quant@quantinium.dev>
2025-06-01 01:02:23 +0530

    added format date fn

---
+++ src/lib/utils.ts
@@ -0,0 +1,8 @@
+type DateStyle = Intl.DateTimeFormatOptions['dateStyle']
+
+export function formatDate(date: string, dateStyle: DateStyle = 'medium', locales = 'en') {
+    const dateToFormat = new Date(date.replaceAll('-', '/'))
+    const dateFormatter = Intl.DateTimeFormat(locales, { dateStyle });
+    return dateFormatter.format(dateToFormat);
+}
+
