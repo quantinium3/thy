5e2edefd06b3a5c310940f12aae473ff47eabfde
quantinium03 <quant@quantinium.dev>
2025-06-01 00:37:20 +0530

    config

---
+++ src/lib/config.ts
@@ -0,0 +1,5 @@
+import { dev } from '$app/environment';
+
+export const title = "Quantinium"
+export const description = "Quantinium"
+export const url = dev ? 'http://localhost:5173/' : 'https://quantinium.dev';
