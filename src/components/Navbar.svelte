bac167fbf8464fb9a539faf3c6ad58eb7f807f63
quantinium03 <quant@quantinium.dev>
2025-06-01 15:06:44 +0530

    fixed navbar and made it simpler

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -24,39 +24,37 @@
 			name: 'resources',
 			href: '/resource'
 		},
-        {
-            name: 'frens',
-            href: '/fren'
-        }
+		{
+			name: 'frens',
+			href: '/fren'
+		}
 	];
 </script>
 
-<div class="my-5 flex flex-col justify-between py-3 dark:text-white">
-		<pre class="text-[0.4rem] lg:text-xs leading-none text-center">
-  █████   █    ██  ▄▄▄       ███▄    █ ▄▄▄█████▓ ██▓ ███▄    █  ██▓ █    ██  ███▄ ▄███▓
-▒██▓  ██▒ ██  ▓██▒▒████▄     ██ ▀█   █ ▓  ██▒ ▓▒▓██▒ ██ ▀█   █ ▓██▒ ██  ▓██▒▓██▒▀█▀ ██▒
-▒██▒  ██░▓██  ▒██░▒██  ▀█▄  ▓██  ▀█ ██▒▒ ▓██░ ▒░▒██▒▓██  ▀█ ██▒▒██▒▓██  ▒██░▓██    ▓██░
-░██  █▀ ░▓▓█  ░██░░██▄▄▄▄██ ▓██▒  ▐▌██▒░ ▓██▓ ░ ░██░▓██▒  ▐▌██▒░██░▓▓█  ░██░▒██    ▒██ 
-░▒███▒█▄ ▒▒█████▓  ▓█   ▓██▒▒██░   ▓██░  ▒██▒ ░ ░██░▒██░   ▓██░░██░▒▒█████▓ ▒██▒   ░██▒
-░░ ▒▒░ ▒ ░▒▓▒ ▒ ▒  ▒▒   ▓▒█░░ ▒░   ▒ ▒   ▒ ░░   ░▓  ░ ▒░   ▒ ▒ ░▓  ░▒▓▒ ▒ ▒ ░ ▒░   ░  ░
- ░ ▒░  ░ ░░▒░ ░ ░   ▒   ▒▒ ░░ ░░   ░ ▒░    ░     ▒ ░░ ░░   ░ ▒░ ▒ ░░░▒░ ░ ░ ░  ░      ░
-   ░   ░  ░░░ ░ ░   ░   ▒      ░   ░ ░   ░       ▒ ░   ░   ░ ░  ▒ ░ ░░░ ░ ░ ░      ░   
-    ░       ░           ░  ░         ░           ░           ░  ░     ░            ░   
-    </pre>
-	<div class="flex flex-wrap gap-3 justify-center">
+<div class="my-5 flex justify-between py-3 dark:text-white">
+	<div class="flex items-center text-lg">
+		<span class="mr-2">&gt; quantinium</span>
+		<span class="animate-pulse">|</span>
+	</div>
+	<div class="flex flex-wrap justify-center gap-3">
 		{#each links as link (link)}
-			<span>[<a href={link.href} class="hover:underline hover:underline-offset-4">{link.name}</a>]</span>
+			<span
+				>[<a href={link.href} class="hover:underline hover:underline-offset-4">{link.name}</a
+				>]</span
+			>
 		{/each}
-		<button onclick={theme.toggle} aria-label="Toggle theme">
-			{#if theme.current === 'dark'}
-				<div in:fly={{ y: 10 }}>
-					<Sun />
-				</div>
-			{:else}
-				<div in:fly={{ y: -10 }}>
-					<Moon />
-				</div>
-			{/if}
-		</button>
+		<div>
+			<button onclick={theme.toggle} aria-label="Toggle theme">
+				{#if theme.current === 'dark'}
+					<div in:fly={{ y: 10 }}>
+						<Sun />
+					</div>
+				{:else}
+					<div in:fly={{ y: -10 }}>
+						<Moon />
+					</div>
+				{/if}
+			</button>
+		</div>
 	</div>
 </div>
