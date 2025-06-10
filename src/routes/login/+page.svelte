63589da5d67cfcfe024734ae06f24594158c7ba7
quantinium03 <quant@quantinium.dev>
2025-06-10 07:36:37 +0530

    login page

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,75 +1,39 @@
 <script lang="ts">
 	import * as config from '$lib/config';
-	import { fly } from 'svelte/transition';
-	import { Moon, Sun } from '@lucide/svelte';
-	import { theme } from '$lib/theme.svelte';
-
-	let links = [
-		{
-			name: 'home',
-			href: '/'
-		},
-		{
-			name: 'blog',
-			href: '/blog'
-		},
-		{
-			name: 'projects',
-			href: '/project'
-		},
-		{
-			name: 'guides',
-			href: '/guide'
-		},
-		{
-			name: 'resources',
-			href: '/resource'
-		},
-		{
-			name: 'frens',
-			href: '/fren'
-		},
-        {
-            name: 'login',
-            href: '/login'
-        }
-	];
 </script>
 
 <svelte:head>
 	<title>{config.title}</title>
 </svelte:head>
 
-<div class="my-5 flex flex-col justify-between py-3 dark:text-white">
-	<pre class="text-center text-[0.4rem] leading-none lg:text-xs">
- ██▓     ▒█████    ▄████  ██▓ ███▄    █ 
-▓██▒    ▒██▒  ██▒ ██▒ ▀█▒▓██▒ ██ ▀█   █ 
-▒██░    ▒██░  ██▒▒██░▄▄▄░▒██▒▓██  ▀█ ██▒
-▒██░    ▒██   ██░░▓█  ██▓░██░▓██▒  ▐▌██▒
-░██████▒░ ████▓▒░░▒▓███▀▒░██░▒██░   ▓██░
-░ ▒░▓  ░░ ▒░▒░▒░  ░▒   ▒ ░▓  ░ ▒░   ▒ ▒ 
-░ ░ ▒  ░  ░ ▒ ▒░   ░   ░  ▒ ░░ ░░   ░ ▒░
-  ░ ░   ░ ░ ░ ▒  ░ ░   ░  ▒ ░   ░   ░ ░ 
-    ░  ░    ░ ░        ░  ░           ░ 
-    </pre>
-	<div class="flex flex-wrap justify-center gap-3">
-		{#each links as link (link)}
-			<span
-				>[<a href={link.href} class="hover:underline hover:underline-offset-4">{link.name}</a
-				>]</span
-			>
-		{/each}
-		<button onclick={theme.toggle} aria-label="Toggle theme">
-			{#if theme.current === 'dark'}
-				<div in:fly={{ y: 10 }}>
-					<Sun />
-				</div>
-			{:else}
-				<div in:fly={{ y: -10 }}>
-					<Moon />
-				</div>
-			{/if}
-		</button>
-	</div>
+<div class="flex items-center justify-center flex-col mt-9">
+	<img src="/login.png" class="w-72"  alt="lain"/>
+		<h1 class="mt-3 md:pt-12 font-bold py-5 text-xl">Login</h1>
+		<div class="flex flex-col gap-3">
+			<form method="POST" action="#" class="flex flex-col gap-3">
+				<label>
+					username
+					<input name="username" type="text"
+								 class="border dark:border-white border-black rounded-md px-1" />
+				</label>
+				<label>
+					password
+					<input name="password" type="password"
+								 class="border dark:border-white border-black rounded-md px-1"/>
+				</label>
+				<button formaction="#"
+								class="rounded-md border dark:border-white p-1">log
+					in</button>
+			</form>
+		</div>
 </div>
 
+<style>
+pre {
+    font-size: 4px;
+		line-height: 100%;
+		color: #000000;
+		padding: 4px 5px;
+		--fs: 11px;
+}
+</style>
