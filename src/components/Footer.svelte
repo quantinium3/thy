1a57930a2f591bc6d98be597d5d20ccdc4f7b3d4
quantinium03 <quant@quantinium.dev>
2025-06-10 07:52:31 +0530

    added links

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,11 +1,17 @@
 <script lang="ts">
-	import {Github, Linkedin, Rss} from '@lucide/svelte';
+	import {Github, Twitter, Rss} from '@lucide/svelte';
 </script>
-<footer class="flex justify-betwee4">
+<footer class="flex justify-between py-5">
 	<div>quantinium @{new Date().getFullYear()}</div>
 	<div class="flex gap-2">
-		<Rss />
-		<Github />
-		<Linkedin />
+		<a href="/rss-xml" target="_blank">
+			<Rss />
+		</a>
+		<a href="https://github.com/quantinium3" target="_blank">
+			<Github />
+		</a>
+		<a href="https://x.com/quantinium3" target="_blank">
+			<Twitter />
+		</a>
 	</div>
 </footer>
