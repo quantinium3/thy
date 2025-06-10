5bad3d0ee1715feb887559e88e705ecd3ddb72ac
quantinium03 <quant@quantinium.dev>
2025-06-10 07:36:19 +0530

    moved navbar into layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
 	import '../app.css';
+	import Navbar from '$components/Navbar.svelte';
+	import NavSidebar from '$components/NavSidebar.svelte';
+	import Footer from '$components/Footer.svelte';
 
 	let { children } = $props();
+
 </script>
 
+<Navbar />
 {@render children()}
