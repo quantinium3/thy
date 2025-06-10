58ac48772a72a78fcb46e944a5785231976433c4
quantinium03 <quant@quantinium.dev>
2025-06-10 07:36:53 +0530

    footer component

---
+++ src/components/Footer.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import {Github, Linkedin, Rss} from '@lucide/svelte';
+</script>
+<footer class="flex justify-betwee4">
+	<div>quantinium @{new Date().getFullYear()}</div>
+	<div class="flex gap-2">
+		<Rss />
+		<Github />
+		<Linkedin />
+	</div>
+</footer>
