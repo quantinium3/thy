fdc0757127a35f361ae4af06316aa8f4695ed231
quantinium03 <quant@quantinium.dev>
2025-06-10 08:02:50 +0530

    project page

---
+++ src/components/Project.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+	import { formatDate } from '$lib/utils';
+	let { project } = $props();
+</script>
+
+<div class="py-9 border-b border-dashed flex flex-col gap-5">
+	<div class="" >
+		<h1
+			class="font-semibold text-lg"><a href={project.slug}
+																			 class="link-animate text-[#cc241d] dark:text-[#fb4934] dark:hover:border-b-white">{project.title}</a></h1>
+		<p
+			class="text-xs dark:text-zinc-100 text-zinc-900">{formatDate(blog.date)}</p>
+	</div>
+	<img src={project.image} alt={project.title} />
+	<div class="flex gap-3">
+		{#each project.tech as tech (tech)}
+			<span class="text-xs">#{tech.replaceAll("_", " ")}</span>
+		{/each}
+	</div>
+
+	<div class="text-sm">{project.description}</div>
+	<div><a href="/project/{project.slug}"
+					class="hover:underline underline-offset-2 hover:font-semibold">read
+		more...</a></div>
+</div>
+
+
+<style>
+    .link-animate	{
+        position: relative;
+        text-decoration: none;
+        padding-bottom: 3px;
+    }
+
+    .link-animate:hover {
+        right: 0;
+        text-decoration: none;
+    }
+
+    .link-animate:hover:after {
+        right: 0;
+    }
+
+    .link-animate:after {
+        border-radius: 1em;
+        border-top: .1em solid;
+        content: "";
+        position: absolute;
+        right: 100%;
+        bottom: .14em;
+        left: 0;
+        transition: right .4s cubic-bezier(0,.5,0,1),
+        border-color .4s ease-out;
+    }
+
+    .link-animate:hover:after {
+        right: 0;
+    }
+</style>
--- src/routes/project/+page.svelte
+++ src/routes/project/+page.svelte
@@ -1,76 +1,41 @@
 <script lang="ts">
 	import * as config from '$lib/config';
-	import { fly } from 'svelte/transition';
-	import { Moon, Sun } from '@lucide/svelte';
-	import { theme } from '$lib/theme.svelte';
+	import Project from '$components/Project.svelte';
 
-	let links = [
+	const projects = [
 		{
-			name: 'home',
-			href: '/'
+			title: "Grimoire",
+			description: "Static site generator",
+			categories: ["typescript", "handlebars"],
+			slug: 'https://github.com/quantinium3/grimoire'
 		},
 		{
-			name: 'blog',
-			href: '/blog'
-		},
-		{
-			name: 'projects',
-			href: '/project'
-		},
-		{
-			name: 'guides',
-			href: '/guide'
-		},
-		{
-			name: 'resources',
-			href: '/resource'
-		},
-		{
-			name: 'frens',
-			href: '/fren'
-		},
-        {
-            name: 'login',
-            href: '/login'
-        }
-
-	];
+			title: "Grimoire",
+			description: "Static site generator",
+			categories: ["typescript", "handlebars"],
+			slug: 'https://github.com/quantinium3/grimoire'
+		}
+	]
 </script>
 
 <svelte:head>
 	<title>{config.title}</title>
 </svelte:head>
 
-<div class="my-5 flex flex-col justify-between py-3 dark:text-white">
-	<pre class="text-center text-[0.4rem] leading-none lg:text-xs">
- ██▓███   ██▀███   ▒█████   ▄▄▄██▀▀▀▓█████  ▄████▄  ▄▄▄█████▓  ██████ 
-▓██░  ██▒▓██ ▒ ██▒▒██▒  ██▒   ▒██   ▓█   ▀ ▒██▀ ▀█  ▓  ██▒ ▓▒▒██    ▒ 
-▓██░ ██▓▒▓██ ░▄█ ▒▒██░  ██▒   ░██   ▒███   ▒▓█    ▄ ▒ ▓██░ ▒░░ ▓██▄   
-▒██▄█▓▒ ▒▒██▀▀█▄  ▒██   ██░▓██▄██▓  ▒▓█  ▄ ▒▓▓▄ ▄██▒░ ▓██▓ ░   ▒   ██▒
-▒██▒ ░  ░░██▓ ▒██▒░ ████▓▒░ ▓███▒   ░▒████▒▒ ▓███▀ ░  ▒██▒ ░ ▒██████▒▒
-▒▓▒░ ░  ░░ ▒▓ ░▒▓░░ ▒░▒░▒░  ▒▓▒▒░   ░░ ▒░ ░░ ░▒ ▒  ░  ▒ ░░   ▒ ▒▓▒ ▒ ░
-░▒ ░       ░▒ ░ ▒░  ░ ▒ ▒░  ▒ ░▒░    ░ ░  ░  ░  ▒       ░    ░ ░▒  ░ ░
-░░         ░░   ░ ░ ░ ░ ▒   ░ ░ ░      ░   ░          ░      ░  ░  ░  
-            ░         ░ ░   ░   ░      ░  ░░ ░                     ░  
-    </pre>
-	<div class="flex flex-wrap justify-center gap-3">
-		{#each links as link (link)}
-			<span
-				>[<a href={link.href} class="hover:underline hover:underline-offset-4">{link.name}</a
-				>]</span
-			>
+<div class="flex">
+	<div class="md:flex-3/4 pr-0 md:pr-12">
+		{#each projects as project (project)}
+			<Project {project}/>
 		{/each}
-		<button onclick={theme.toggle} aria-label="Toggle theme">
-			{#if theme.current === 'dark'}
-				<div in:fly={{ y: 10 }}>
-					<Sun />
+	</div>
+
+	<div class="py-9 px-0 md:px-3 flex-1/4 hidden md:block">
+		{#each projects as project (project.title)}
+			<div class="flex">
+				<div class="hover:underline hover:underline-offset-4 hover:font-semibold" >
+					-> {project.title}
 				</div>
-			{:else}
-				<div in:fly={{ y: -10 }}>
-					<Moon />
-				</div>
-			{/if}
-		</button>
+			</div>
+		{/each}
 	</div>
 </div>
-
