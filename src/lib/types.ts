7d9218494e134b96195b079a2932dc5181e1f28c
quantinium03 <quant@quantinium.dev>
2025-06-17 12:59:27 +0530

    new types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -8,3 +8,38 @@ export type Blog = {
     categories: Categories[]
     published: boolean
 }
+
+export type ChartDataSet = {
+	name: string
+	percent: number
+	color: string
+}
+
+export type Dataset = {
+	labels: string[];
+	datasets: {
+		label: string;
+		data: number[];
+		backgroundColor: string[];
+		borderColor: string[],
+		hoverOffset: number;
+	}[];
+}
+
+export type LineData = {
+	grand_total: {
+		decimal: string;
+		digital: string;
+		hours: number;
+		minutes: number;
+		text: string;
+		total_seconds: number;
+	};
+	range: {
+		date: string;
+		end: string;
+		start: string;
+		text: string;
+		timezone: string;
+	};
+}
