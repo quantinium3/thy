f3d6885c18b63e8f05f721be7e738d0d7209b0a5
quantinium03 <quant@quantinium.dev>
2025-06-17 17:13:39 +0530

    projects

--- src/routes/project/+page.svelte
+++ src/routes/project/+page.svelte
@@ -5,13 +5,73 @@
 	const projects = [
 		{
 			title: "Grimoire",
-			description: "Static site generator",
+			description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto rem earum doloribus atque temporibus veritatis cum aliquam inventore animi consequuntur ad reiciendis nisi asperiores, id laudantium repellendus, recusandae illo vitae?",
 			tech: ["typescript", "handlebars"],
 			slug: "https://github.com/quantinium3/grimoire",
 		},
 		{
 			title: "lomes",
-			description: "local media server",
+			description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto rem earum doloribus atque temporibus veritatis cum aliquam inventore animi consequuntur ad reiciendis nisi asperiores, id laudantium repellendus, recusandae illo vitae?",
+			tech: ["typescript", "reactjs"],
+			slug: "https://github.com/quantinium3/lomes"
+		},
+		{
+			title: "nvgs",
+			description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto rem earum doloribus atque temporibus veritatis cum aliquam inventore animi consequuntur ad reiciendis nisi asperiores, id laudantium repellendus, recusandae illo vitae?",
+			tech: ["rust", "reactjs"],
+			slug: "https://github.com/quantinium3/rust"
+		},
+		{
+			title: "tsh",
+			description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto rem earum doloribus atque temporibus veritatis cum aliquam inventore animi consequuntur ad reiciendis nisi asperiores, id laudantium repellendus, recusandae illo vitae?",
+			tech: ["typescript", "reactjs"],
+			slug: "https://github.com/quantinium3/tsh"
+		},
+		{
+			title: "kaze",
+			description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto rem earum doloribus atque temporibus veritatis cum aliquam inventore animi consequuntur ad reiciendis nisi asperiores, id laudantium repellendus, recusandae illo vitae?",
+			tech: ["typescript", "reactjs"],
+			slug: "https://github.com/quantinium3/lomes"
+		},
+		{
+			title: "tanshilink",
+			description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto rem earum doloribus atque temporibus veritatis cum aliquam inventore animi consequuntur ad reiciendis nisi asperiores, id laudantium repellendus, recusandae illo vitae?",
+			tech: ["typescript", "reactjs"],
+			slug: "https://github.com/quantinium3/lomes"
+		},
+		{
+			title: "asami",
+			description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto rem earum doloribus atque temporibus veritatis cum aliquam inventore animi consequuntur ad reiciendis nisi asperiores, id laudantium repellendus, recusandae illo vitae?",
+			tech: ["typescript", "reactjs"],
+			slug: "https://github.com/quantinium3/lomes"
+		},
+		{
+			title: "lated",
+			description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto rem earum doloribus atque temporibus veritatis cum aliquam inventore animi consequuntur ad reiciendis nisi asperiores, id laudantium repellendus, recusandae illo vitae?",
+			tech: ["typescript", "reactjs"],
+			slug: "https://github.com/quantinium3/lomes"
+		},
+		{
+			title: "shiro",
+			description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto rem earum doloribus atque temporibus veritatis cum aliquam inventore animi consequuntur ad reiciendis nisi asperiores, id laudantium repellendus, recusandae illo vitae?",
+			tech: ["typescript", "reactjs"],
+			slug: "https://github.com/quantinium3/lomes"
+		},
+		{
+			title: "yumi",
+			description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto rem earum doloribus atque temporibus veritatis cum aliquam inventore animi consequuntur ad reiciendis nisi asperiores, id laudantium repellendus, recusandae illo vitae?",
+			tech: ["typescript", "reactjs"],
+			slug: "https://github.com/quantinium3/lomes"
+		},
+		{
+			title: "comphub",
+			description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto rem earum doloribus atque temporibus veritatis cum aliquam inventore animi consequuntur ad reiciendis nisi asperiores, id laudantium repellendus, recusandae illo vitae?",
+			tech: ["typescript", "reactjs"],
+			slug: "https://github.com/quantinium3/lomes"
+		},
+		{
+			title: "ryu",
+			description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto rem earum doloribus atque temporibus veritatis cum aliquam inventore animi consequuntur ad reiciendis nisi asperiores, id laudantium repellendus, recusandae illo vitae?",
 			tech: ["typescript", "reactjs"],
 			slug: "https://github.com/quantinium3/lomes"
 		}
@@ -23,13 +83,13 @@
 </svelte:head>
 
 <div class="flex">
-	<div class="md:flex-3/4 pr-0 md:pr-12">
+	<div class="md:flex-3 pr-0 md:pr-12">
 		{#each projects as project (project.title)}
 			<Project project={project}/>
 		{/each}
 	</div>
 
-	<div class="py-9 px-0 md:px-3 flex-1/4 hidden md:block">
+	<div class="py-9 px-0 md:px-3 flex-1 hidden md:block">
 		{#each projects as project (project.title)}
 			<div class="flex">
 				<div class="hover:underline hover:underline-offset-4 hover:font-semibold" >
