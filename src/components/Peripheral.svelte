fbc110e6d4f8e746bf3058ff107c64b3ebe421b7
quantinium3 <quant@quantinium.dev>
2025-12-27 14:15:07 +0530

    added mouse travel

--- src/components/Peripheral.svelte
+++ src/components/Peripheral.svelte
@@ -1,16 +1,15 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-	const formatter = new Intl.NumberFormat('en', {notation: "compact",
-		compactDisplay: "short"})
+	const formatter = new Intl.NumberFormat('en', { notation: 'compact', compactDisplay: 'short' });
 
 	let keypress = $state(0);
 	let right_click = $state(0);
 	let left_click = $state(0);
 	let mouse_travel = $state(0);
+	let scroll_travel = $state(0);
 
-
-	onMount(async() => {
-		const statsUri = import.meta.env.VITE_STATS_URI;
+	onMount(async () => {
+		const statsUri = `${import.meta.env.VITE_NIXIE_URI}/api/stats/${import.meta.env.VITE_USERID}`;
 		if (!statsUri) {
 			throw new Error('Stats URI is missing');
 		}
@@ -18,15 +17,17 @@
 		const statsRes = await fetch(statsUri);
 		const statsData = await statsRes.json();
 
-		keypress = statsData.stats[0].keypress;
-		mouse_travel = statsData.stats[0].mouse_travel;
-		left_click = statsData.stats[0].left_click;
-		right_click = statsData.stats[0].right_click;
-	})
-
+		keypress = statsData.stats.keypress;
+		mouse_travel = statsData.stats.mouse_distance;
+		scroll_travel = statsData.stats.scroll_distance;
+		left_click = statsData.stats.left_click;
+		right_click = statsData.stats.right_click;
+	});
 </script>
-<div class="text-lg font-bold mb-2">Input</div>
-<div class="hour"> > keypress - {formatter.format(keypress)}</div>
-<div class="daily_avg"> > mouse travel - {formatter.format(mouse_travel)} m</div>
-<div class="hour"> > left click - {formatter.format(left_click)}</div>
-<div class="daily_avg"> > right click - {formatter.format(right_click)}</div>
+
+<div class="mb-2 text-lg font-bold">Input</div>
+<div class="hour">> keypress - {formatter.format(keypress)}</div>
+<div class="daily_avg">> mouse travel - {formatter.format(mouse_travel)}m</div>
+<div class="daily_avg">> scroll travel - {formatter.format(scroll_travel)}m</div>
+<div class="hour">> left click - {formatter.format(left_click)}</div>
+<div class="daily_avg">> right click - {formatter.format(right_click)}</div>
