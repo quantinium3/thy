91bc57675bb76862249cd0891187ff8ecedf5a7d
quantinium3 <quant@quantinium.dev>
2026-03-14 01:43:38 +0530

    we back to bun and deployed using wrangler cli

--- src/app.d.ts
+++ src/app.d.ts
@@ -3,7 +3,14 @@ declare global {
 	namespace App {
 		interface Locals {
 			user: import('$lib/server/auth').SessionValidationResult['user'];
-			session: import('$lib/server/auth').SessionValidationResult['session'];
+			session: import('$lib/server/auth').SessionValidationResult['session']
+		}
+
+		interface Platform {
+			env: Env;
+			ctx: ExecutionContext;
+			caches: CacheStorage;
+			cf?: IncomingRequestCfProperties
 		}
 	}
 }
