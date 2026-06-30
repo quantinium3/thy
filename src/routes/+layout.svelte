b375a746359f80a44a86ae2f15b885a8d0b019f2
quantinium3 <quant@quantinium.dev>
2026-06-30 23:08:46 +0530

    bad cdn

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.jpg';
+	import ogGif from "$lib/assets/og.gif"
 	import Navbar from '$lib/components/navbar.svelte';
 
 	let { children } = $props();
@@ -14,13 +15,13 @@
 	<meta property="og:description" content="quantinium's personal website">
 	<meta property="og:type" content="website">
 	<meta property="og:url" content="https://quantinium.dev">
-	<meta property="og:image" content="https://x2onawsk55.ufs.sh/f/eZ86CwF4BrGdAvv9OSxAcYT9SOR5xq8UkvgJl3HFIE0LfsCX">
+	<meta property="og:image" content={ogGif}>
 	<meta property="og:image:type" content="image/gif">
 
 	<meta name="twitter:card" content="summary_large_image">
 	<meta name="twitter:title" content="quantinium">
 	<meta name="twitter:description" content="quantinium's personal website">
-	<meta name="twitter:image" content="https://x2onawsk55.ufs.sh/f/eZ86CwF4BrGdAvv9OSxAcYT9SOR5xq8UkvgJl3HFIE0LfsCX">
+	<meta name="twitter:image" content={ogGif} >
 	<meta name="twitter:domain" content="quantinium.dev">
 	<meta name="twitter:url" content="https://quantinium.dev">
 
